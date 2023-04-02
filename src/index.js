2191b6862808231c61963ca300caa1dedfbbdf17
AndriiSolodaiev <andik05016@gmail.com>
2023-04-02 18:37:30 +0300

    repair packagejson

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="goit-cw-react-04">
           <App />
         </BrowserRouter>
       </PersistGate>
